03cf8bbe64e4a092ca283dfbdc37e35d5c58e36a
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-04 20:41:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,11 @@
 def fizzbuzz(int)
   if int % 3 && 5 == 0
     "FizzBuzz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   elsif int % 3  == 0
     "Fizz"
   else int 3 || 5 == false
-    "nil"
+    nil
   end
 end
